1d6b311a2a82a22f637e609aee06ce67b8370342
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-10 23:24:10 +0000

    automatically backed up by learn

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -1,5 +1,9 @@
- username, password, and balance (a decimal storing their bank account balance)
-
- class CreateUsersTable
-   
+ class CreateUsersTable < ActiveRecord::Migration[5.0]
+   def change
+     create_table :users do |t|
+       t.string :username
+       t.string :password
+       t.decimal :balance
+     end
+   end
  end
