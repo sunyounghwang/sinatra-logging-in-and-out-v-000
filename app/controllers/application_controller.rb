098c1b9af3881160b869fff36fd63d7f45072755
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-10 23:40:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     erb :error
   end
 
-  get "/account" do
+  get '/account/:id' do
     @user = User.find_by_id(session[:id])
     erb :account
   end
