12586a15bfb5ba5424db5e82050f1e6427a5c32c
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-10 23:24:53 +0000

    automatically backed up by learn

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -1,9 +1,9 @@
- class CreateUsersTable
-   def change
-     create_table :users do |t|
-       t.string :username
-       t.string :password
-       t.decimal :balance
-     end
+class CreateUsersTable
+ def change
+   create_table :users do |t|
+     t.string :username
+     t.string :password
+     t.decimal :balance
    end
  end
+end
