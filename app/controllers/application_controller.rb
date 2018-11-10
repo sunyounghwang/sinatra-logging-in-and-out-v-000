0f94cad67161e490c51053f210a8da78caeb09d7
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-10 23:31:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
+    session[:id] = @user.id 
   end
 
   get '/account' do
